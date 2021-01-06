4507bbe04bc06b5fac0670b0192beac95f2313fc
Adam Fernandes <fernadam8@gmail.com>
2021-01-06 17:58:27 -0500

    custom types and filler div

---
+++ frontend/src/CustomTypes.tsx
@@ -0,0 +1,7 @@
+/**
+ * Signifies an object with an attribute of URL
+ */
+export type urlProp = {
+  url: string;
+  hyperlinkName: string;
+};
---
+++ frontend/src/Filler.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+/**
+ * Div with flex 1 as filler
+ */
+const Filler = () => {
+  return <div style={{ flex: 1 }}></div>;
+};
+
+export default Filler;
