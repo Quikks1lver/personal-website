486ad6900b8a2c334fc60494745e5a61ca0a0ceb
Adam Fernandes <fernadam8@gmail.com>
2021-01-06 10:20:39 -0500

    started header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 const App = () => {
   const welcome: string = "Hi";
 
   return (
     <div>
+      <Header />
       <h1>{welcome}</h1>
     </div>
   );
