468a3e93b02726c5f252d593bc264718ccd15cad
Adam Fernandes <fernadam8@gmail.com>
2021-01-06 14:19:14 -0500

    start of footer and intro blurb

---
+++ frontend/src/IntroBlurb.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+const IntroBlurb = () => {
+  return (
+    <div>
+      <h1 className="blurb">
+        <span>What's up, I'm </span>
+        <span style={{ color: "gold" }}>Adam</span>
+      </h1>
+    </div>
+  );
+};
+
+export default IntroBlurb;
