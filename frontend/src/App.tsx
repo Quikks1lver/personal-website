588e87058f05f57d1f5cf5147312724bf03cbdb5
Adam Fernandes <57380803+Quikks1lver@users.noreply.github.com>
2021-01-07 13:34:43 -0500

    site is up and "routing" :p (#4)
    * hash routing working xD
    
    * added projects and resume templates
    
    * about me and contact me
    
    * header button prop type
    
    * removed home button, added more customization for headerbutton component
    
    * wording fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,11 @@ import { urlProp } from "./CustomTypes";
 import Footer from "./Footer";
 import Header from "./Header";
 import IntroBlurb from "./IntroBlurb";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Resume from "./Resume";
+import Projects from "./Projects";
+import ContactMe from "./ContactMe";
+import AboutMe from "./AboutMe";
 
 const App = () => {
   // Footer props
@@ -14,11 +19,17 @@ const App = () => {
   };
 
   return (
-    <div>
+    <Router basename="/">
       <Header />
-      <IntroBlurb />
+
+      <Route exact path="/" component={IntroBlurb} />
+      <Route exact path="/aboutme" component={AboutMe} />
+      <Route exact path="/contact" component={ContactMe} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/resume" component={Resume} />
+
       <Footer url={footerProps.url} hyperlinkName={footerProps.hyperlinkName} />
-    </div>
+    </Router>
   );
 };
 
