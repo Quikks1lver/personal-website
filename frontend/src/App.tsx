d2d29ce66c89c89f8f72cc63279f05e24f0c5335
Adam Fernandes <fernadam8@gmail.com>
2021-01-07 12:36:00 -0500

    about me and contact me

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,8 @@ import IntroBlurb from "./IntroBlurb";
 import { HashRouter as Router, Route } from "react-router-dom";
 import Resume from "./Resume";
 import Projects from "./Projects";
+import ContactMe from "./ContactMe";
+import AboutMe from "./AboutMe";
 
 const App = () => {
   // Footer props
@@ -21,8 +23,10 @@ const App = () => {
       <Header />
 
       <Route exact path="/" component={IntroBlurb} />
-      <Route path="/resume" component={Resume} />
+      <Route exact path="/aboutme" component={AboutMe} />
+      <Route exact path="/contact" component={ContactMe} />
       <Route path="/projects" component={Projects} />
+      <Route path="/resume" component={Resume} />
 
       <Footer url={footerProps.url} hyperlinkName={footerProps.hyperlinkName} />
     </Router>
