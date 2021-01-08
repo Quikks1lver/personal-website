539b2bfcf507c4d422235abc639c64c989622e11
Adam Fernandes <fernadam8@gmail.com>
2021-01-07 23:34:09 -0500

    put footer props in its own file and resource

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import { urlProp } from "./CustomTypes";
-import Footer from "./Footer";
-import Header from "./Header";
-import IntroBlurb from "./IntroBlurb";
 import { HashRouter as Router, Route } from "react-router-dom";
-import Resume from "./Resume";
-import Projects from "./Projects";
-import ContactMe from "./ContactMe";
 import AboutMe from "./AboutMe";
-import { useMediaQuery } from "./useMediaQuery";
-import burger from "./images/burger.png";
-import Filler from "./Filler";
-
-// Footer props
-const GITHUB_URL: string = "https://github.com/Quikks1lver/personal-website";
-const footerProps: urlProp = {
-  url: GITHUB_URL,
-  hyperlinkName: "Website Source Code",
-};
+import ContactMe from "./ContactMe";
+import Footer from "./Footer";
+import footerProps from "./FooterProps";
+import IntroBlurb from "./IntroBlurb";
+import Projects from "./Projects";
+import ResponsiveHeader from "./ResponsiveHeader";
+import Resume from "./Resume";
 
 const App = () => {
-  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
-  const [isHamburgerHoveredUpon, setIsHamburgerHoveredUpon] = useState(false);
-  let isMobile = useMediaQuery("(max-width: 425px)");
-
   return (
     <Router basename="/">
-      {isMobile && (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <button
-            className={
-              isHamburgerHoveredUpon
-                ? "hamburger-button-hover"
-                : "hamburger-button"
-            }
-            onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}
-            onMouseEnter={() => setIsHamburgerHoveredUpon(true)}
-            onMouseLeave={() => setIsHamburgerHoveredUpon(false)}
-          >
-            <img src={burger} style={{ alignContent: "center" }}></img>
-          </button>
-        </div>
-      )}
-      {(isHamburgerOpen && <Header />) || (!isMobile && <Header />)}
+      <ResponsiveHeader />
 
       <Route exact path="/" component={IntroBlurb} />
       <Route exact path="/aboutme" component={AboutMe} />
---
+++ frontend/src/FooterProps.tsx
@@ -0,0 +1,9 @@
+import { urlProp } from "./CustomTypes";
+
+const GITHUB_URL: string = "https://github.com/Quikks1lver/personal-website";
+const footerProps: urlProp = {
+  url: GITHUB_URL,
+  hyperlinkName: "Website Source Code",
+};
+
+export default footerProps;
