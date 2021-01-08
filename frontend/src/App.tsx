6092dfc4e7f91980b4ba585d0684310a8b39a88c
Adam Fernandes <fernadam8@gmail.com>
2021-01-07 23:22:26 -0500

    burger button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { urlProp } from "./CustomTypes";
 import Footer from "./Footer";
@@ -9,18 +9,41 @@ import Resume from "./Resume";
 import Projects from "./Projects";
 import ContactMe from "./ContactMe";
 import AboutMe from "./AboutMe";
+import { useMediaQuery } from "./useMediaQuery";
+import burger from "./images/burger.png";
+import Filler from "./Filler";
+
+// Footer props
+const GITHUB_URL: string = "https://github.com/Quikks1lver/personal-website";
+const footerProps: urlProp = {
+  url: GITHUB_URL,
+  hyperlinkName: "Website Source Code",
+};
 
 const App = () => {
-  // Footer props
-  const GITHUB_URL: string = "https://github.com/Quikks1lver/personal-website";
-  const footerProps: urlProp = {
-    url: GITHUB_URL,
-    hyperlinkName: "Website Source Code",
-  };
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+  const [isHamburgerHoveredUpon, setIsHamburgerHoveredUpon] = useState(false);
+  let isMobile = useMediaQuery("(max-width: 425px)");
 
   return (
     <Router basename="/">
-      <Header />
+      {isMobile && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button
+            className={
+              isHamburgerHoveredUpon
+                ? "hamburger-button-hover"
+                : "hamburger-button"
+            }
+            onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}
+            onMouseEnter={() => setIsHamburgerHoveredUpon(true)}
+            onMouseLeave={() => setIsHamburgerHoveredUpon(false)}
+          >
+            <img src={burger} style={{ alignContent: "center" }}></img>
+          </button>
+        </div>
+      )}
+      {(isHamburgerOpen && <Header />) || (!isMobile && <Header />)}
 
       <Route exact path="/" component={IntroBlurb} />
       <Route exact path="/aboutme" component={AboutMe} />
