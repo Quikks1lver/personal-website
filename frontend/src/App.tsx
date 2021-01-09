781eeb879ce94030992975890b30f57f68971cec
Adam Fernandes <fernadam8@gmail.com>
2021-01-09 18:49:56 -0500

    removed footer props file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,19 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import AboutMe from "./AboutMe";
 import ContactMe from "./ContactMe";
 import Footer from "./Footer";
-import footerProps from "./FooterProps";
 import IntroBlurb from "./IntroBlurb";
 import Projects from "./Projects";
 import ResponsiveHeader from "./ResponsiveHeader";
 import Resume from "./Resume";
+import { urlProp } from "./CustomTypes";
 
 const App = () => {
+  const GITHUB_URL: string = "https://github.com/Quikks1lver/personal-website";
+  const footerProps: urlProp = {
+    url: GITHUB_URL,
+    hyperlinkName: "Developed with <3 by Adam",
+  };
+
   return (
     <Router basename="/">
       <ResponsiveHeader />
