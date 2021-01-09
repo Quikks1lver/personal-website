6bd663098d9d8ab7871b327e377669f7347354f7
Adam Fernandes <fernadam8@gmail.com>
2021-01-09 18:56:56 -0500

    heading JS doc

--- frontend/src/MiniHeading.tsx
+++ frontend/src/MiniHeading.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+/**
+ * A neat little heading component
+ * @param param0 string representing the heading
+ */
 const MiniHeading = ({ heading }: { heading: string }) => {
   return <h2>{heading}</h2>;
 };
