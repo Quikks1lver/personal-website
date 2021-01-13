6f9c10ba18cadfbf8dcfa047ab950794ca7e055e
Adam Fernandes <fernadam8@gmail.com>
2021-01-13 12:58:59 -0500

    hovering effects

--- frontend/src/NewsArticle.tsx
+++ frontend/src/NewsArticle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { MOBILE_WIDTH_THRESHOLD } from "./Constants";
 import { newsArticleProp } from "./CustomTypes";
 import Filler from "./Filler";
@@ -7,24 +7,26 @@ import { useMediaQuery } from "./useMediaQuery";
 const NewsArticle = ({ blurb, image, link }: newsArticleProp) => {
   let renderFillers = useMediaQuery(MOBILE_WIDTH_THRESHOLD);
 
+  const imageStyles: React.CSSProperties = {
+    flex: 1,
+    width: "50px",
+    height: "70px",
+    margin: "1%",
+    alignSelf: "center",
+  };
+
+  const [isHoveredUpon, setIsHoveredUpon] = useState(false);
+
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
       {!renderFillers && <Filler flexSize={1} />}
       <div
-        className="news-container"
+        className={isHoveredUpon ? "hovered-news-container" : "news-container"}
         style={{ display: "flex", flexDirection: "row", flex: 4 }}
+        onMouseEnter={() => setIsHoveredUpon(true)}
+        onMouseLeave={() => setIsHoveredUpon(false)}
       >
-        <img
-          src={image}
-          alt="news article image"
-          style={{
-            flex: 1,
-            width: "50px",
-            height: "70px",
-            margin: "1%",
-            alignSelf: "center",
-          }}
-        ></img>
+        <img src={image} alt="news article image" style={imageStyles}></img>
         <p className="paragraph" style={{ flex: 7 }}>
           <a href={link}>
             <em>
