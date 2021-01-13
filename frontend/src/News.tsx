3cc6f436bb657e9c100f3bb6b5685097e9d2ce5b
Adam Fernandes <fernadam8@gmail.com>
2021-01-13 12:32:18 -0500

    threw info into a file and consulted stack overflow for seeing if a page exists

--- frontend/src/News.tsx
+++ frontend/src/News.tsx
@@ -1,42 +1,43 @@
 import React from "react";
 import MiniHeading from "./MiniHeading";
 import NewsArticle from "./NewsArticle";
-import facebookIcon from "./images/facebook.png";
-import wcasdIcon from "./images/wcasd.png";
-import dailyLocalIcon from "./images/daily_local.png";
+import newsInfo from "./NewsInfo";
 
 const News = () => {
+  const displayArticles = (): JSX.Element[] => {
+    var output: JSX.Element[] = [];
+    newsInfo.forEach((article) => {
+      var success = true;
+
+      // from stack overflow
+      var request = new XMLHttpRequest();
+      request.open("GET", article.link, true);
+      request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+          if (request.status === 404) {
+            success = false;
+          }
+        }
+      };
+
+      if (success) {
+        output.push(
+          <NewsArticle
+            blurb={article.blurb}
+            image={article.image}
+            link={article.link}
+          />
+        );
+      }
+    });
+
+    return output;
+  };
+
   return (
     <div>
       <MiniHeading heading={"News Links"} />
-      <NewsArticle
-        blurb={"February 2019: National Merit Finalist"}
-        image={dailyLocalIcon}
-        link={
-          "https://www.dailylocal.com/news/local/11-west-chester-students-named-national-merit-finalists/article_42eb5986-5543-11e9-88b4-4b8d8657d5cd.html"
-        }
-      />
-      <NewsArticle
-        blurb={"April 2019: Spellman Humanitarian Award"}
-        image={facebookIcon}
-        link={
-          "https://www.facebook.com/WestChesterAreaSchoolDistrict/posts/congratulations-to-the-2019-spellman-humanitarian-winnerseast-high-school-jessic/2317693254917968/"
-        }
-      />
-      <NewsArticle
-        blurb={"May 2019: High School Graduation / Salutatorian Speech"}
-        image={dailyLocalIcon}
-        link={
-          "https://www.dailylocal.com/news/local/weather-perfect-as-312-graduate-from-henderson-high-school/article_f1f68486-89e2-11e9-909f-fbb593093149.html"
-        }
-      />
-      <NewsArticle
-        blurb={"May 2019: Henderson Alumni Scholarship"}
-        image={wcasdIcon}
-        link={
-          "https://www.wcasd.net/site/default.aspx?PageType=3&DomainID=8&ModuleInstanceID=7172&ViewID=6446EE88-D30C-497E-9316-3F8874B3E108&RenderLoc=0&FlexDataID=22450&PageID=9"
-        }
-      />
+      {displayArticles()}
     </div>
   );
 };
