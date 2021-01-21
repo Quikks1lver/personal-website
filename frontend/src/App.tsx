6c8fc6dd8144b2f6cd8bb93a9ee06199ca8df6e4
Adam Fernandes <fernadam8@gmail.com>
2021-01-21 18:08:55 -0500

    yellow heart emoji over <3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
   const GITHUB_URL: string = "https://github.com/Quikks1lver/personal-website";
   const footerProps: urlProp = {
     url: GITHUB_URL,
-    hyperlinkName: "Developed with <3 by Adam",
+    hyperlinkName: "Developed with 💛 by Adam",
   };
 
   return (
