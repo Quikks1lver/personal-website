dab1ef62b1e2728114cff4a31e031c7fb1e119a6
Adam Fernandes <57380803+Quikks1lver@users.noreply.github.com>
2021-01-26 09:45:07 -0500

    ContactMe "Backend" is Up (#11)
    * yellow heart emoji over <3
    
    * removed unnecessary image alt
    
    * API key file
    
    * changed name API Key
    
    * added emailjs package
    
    * updated gitignore
    
    * email keys file
    
    * added bogus fake API keys for git
    
    * functionality for emailJS sending
    
    * gitignore
    
    * gitignore
    
    * removing email apis
    
    * added back API file ... xO
    
    * ignoring emailjs key
    
    * fixed gitignore trouble
    
    * resources
    
    * gitignore grr
    
    * gitignore

--- frontend/src/ContactMe.tsx
+++ frontend/src/ContactMe.tsx
@@ -1,8 +1,53 @@
 import React from "react";
 import MiniHeading from "./MiniHeading";
 
+import emailjs from "emailjs-com";
+import { EmailJS_INFO } from "./EmailJS_Keys";
+
+// Much of the code here is from https://www.emailjs.com/docs/examples/reactjs/, thanks!
+
 const ContactMe = () => {
-  return <MiniHeading heading={"Contact Me"} />;
+  /**
+   * Sends an email through EmailJS
+   * @param Event
+   */
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        EmailJS_INFO.EMAIL_SERVICE,
+        EmailJS_INFO.EMAIL_TEMPLATE,
+        e.target,
+        EmailJS_INFO.USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
+  return (
+    <div>
+      <MiniHeading heading="Contact Me" />
+      <form className="contact-form" onSubmit={sendEmail}>
+        <input type="hidden" name="contact_number" />
+        <label>Name</label>
+        <input type="text" name="name" />
+        <label>Email</label>
+        <input type="email" name="email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
 };
 
 export default ContactMe;
