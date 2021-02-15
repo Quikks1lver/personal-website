1ede2fd8d173f02d27a6a30cbc8887637e8180ba
Adam Fernandes <fernadam8@gmail.com>
2021-02-14 20:23:42 -0500

    added project component boilerplate

---
+++ frontend/src/IndivualProject.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * Individual project component
+ */
+const IndivualProject = () => {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
+};
+
+export default IndivualProject;
--- frontend/src/Projects.tsx
+++ frontend/src/Projects.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import MiniHeading from "./MiniHeading";
 
 const Projects = () => {
-  return <MiniHeading heading={"Projects"} />;
+  return (
+    <div>
+      <MiniHeading heading={"Projects"} />
+    </div>
+  );
 };
 
 export default Projects;
