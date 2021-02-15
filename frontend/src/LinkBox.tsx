360a593f9854c3e9d831e79e67772007713c02d2
Adam Fernandes <fernadam8@gmail.com>
2021-02-15 08:47:00 -0500

    more subdued

--- frontend/src/LinkBox.tsx
+++ frontend/src/LinkBox.tsx
@@ -7,7 +7,7 @@ import { linkBoxProp } from "./CustomTypes";
  */
 const LinkBox = ({ url, image, siteName }: linkBoxProp) => {
   const hoveredStyle: React.CSSProperties = {
-    backgroundColor: "#f8f5ec",
+    backgroundColor: "#de602546",
   };
 
   const [isHoveredUpon, setIsHoveredUpon] = useState(false);
