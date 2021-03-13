e53a9faa9b2e1c9145ce5d0b9958f02666e4046b
Adam Fernandes <fernadam8@gmail.com>
2021-03-12 22:08:38 -0500

    iphone width threshold constant

--- frontend/src/Constants.tsx
+++ frontend/src/Constants.tsx
@@ -1,2 +1,3 @@
 export const MOBILE_WIDTH_THRESHOLD: string = "(max-width: 425px)";
 export const IPAD_WIDTH_THRESHOLD: string = "(max-width: 767px)";
+export const IPHONE_VERTICAL_WIDTH_THRESHOLD: string = "(max-width: 379px)";
