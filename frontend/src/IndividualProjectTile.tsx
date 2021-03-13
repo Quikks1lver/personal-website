349a1a3af9081e2a428457d463c56e5c316e3b99
Adam Fernandes <fernadam8@gmail.com>
2021-03-12 22:09:24 -0500

    css rework

--- frontend/src/IndividualProjectTile.tsx
+++ frontend/src/IndividualProjectTile.tsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  IPAD_WIDTH_THRESHOLD,
+  IPHONE_VERTICAL_WIDTH_THRESHOLD,
+} from "./Constants";
 import { projectProp } from "./CustomTypes";
+import { useMediaQuery } from "./useMediaQuery";
 
-const IndivualProjectTile = ({ picture }: projectProp) => {
+const IndivualProjectTile = ({ picture, route }: projectProp) => {
+  const isIpadOrSmaller = useMediaQuery(IPAD_WIDTH_THRESHOLD);
+  const isiPhoneLengthWise = useMediaQuery(IPHONE_VERTICAL_WIDTH_THRESHOLD);
+
   return (
-    <div
-      style={{ marginLeft: "1%", marginRight: "1%" }}
-      onClick={() => console.log("hi")}
-    >
-      <img className="project-icon" src={picture} alt="project icon" />
+    <div style={{ marginLeft: "1%", marginRight: "1%" }}>
+      <Link to={route}>
+        <img
+          style={
+            isIpadOrSmaller
+              ? isiPhoneLengthWise
+                ? { width: "45vw", height: "23vh" }
+                : { width: "24vw", height: "25vh" }
+              : { border: "none" }
+          }
+          className="project-icon"
+          src={picture}
+          alt="project icon"
+        />
+      </Link>
     </div>
   );
 };
