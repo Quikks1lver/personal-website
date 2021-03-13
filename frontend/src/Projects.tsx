4227cfb384731bc2ff683b27ac0132e4dc4b1eb9
Adam Fernandes <fernadam8@gmail.com>
2021-03-12 19:56:57 -0500

    icon setup projects page

--- frontend/src/Projects.tsx
+++ frontend/src/Projects.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import MiniHeading from "./MiniHeading";
 
+import IndivualProjectTile from "./IndivualProjectTile";
+
+import battleship_icon from "./images/battleship_icon.png";
+import justice_bytes_icon from "./images/justice_bytes_icon.png";
+import movie_icon from "./images/movie_icon.png";
+import pizza_icon from "./images/pizza_icon.png";
+import visa_icon from "./images/visa_icon.png";
+
 const Projects = () => {
   return (
     <div>
       <MiniHeading heading={"Projects"} />
+
+      <div
+        className="project-tiles-container"
+        style={{ justifyContent: "center" }}
+      >
+        <IndivualProjectTile picture={justice_bytes_icon} />
+        <IndivualProjectTile picture={pizza_icon} />
+        <IndivualProjectTile picture={visa_icon} />
+        <IndivualProjectTile picture={movie_icon} />
+        <IndivualProjectTile picture={battleship_icon} />
+      </div>
     </div>
   );
 };
