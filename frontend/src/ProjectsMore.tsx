7d075370473ae78a60d578d0c1e6e7caef8b27ee
Adam Fernandes <fernadam8@gmail.com>
2021-03-12 22:10:13 -0500

    cleaned projects more back button

--- frontend/src/ProjectsMore.tsx
+++ frontend/src/ProjectsMore.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { MOBILE_WIDTH_THRESHOLD } from "./Constants";
+import { useMediaQuery } from "./useMediaQuery";
 
 /**
  * A "return to projects page" button present at the top of each individual project blurb
  * @returns
  */
 const ProjectsMore = () => {
+  const isMobile = useMediaQuery(MOBILE_WIDTH_THRESHOLD);
+
   return (
     <div
       style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        marginTop: "1%",
+        marginTop: "2%",
+        marginBottom: "3%",
       }}
     >
       <Link to="/projects">
-        <button className="projects-more-back-button">{`Return to Projects ↩ `}</button>
+        <button
+          style={isMobile ? { fontSize: "40px" } : undefined}
+          className="projects-more-back-button"
+        >{` ↩ `}</button>
       </Link>
     </div>
   );
