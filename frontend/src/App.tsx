8b53e332c42d107642b9b4ebf0ff5d97fbed0668
Adam Fernandes <fernadam8@gmail.com>
2021-03-14 00:05:58 -0500

    blue heart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
   const GITHUB_URL: string = "https://github.com/Quikks1lver/personal-website";
   const footerProps: urlProp = {
     url: GITHUB_URL,
-    hyperlinkName: "Developed with 💛 by Adam",
+    hyperlinkName: "Developed with 💙 by Adam",
   };
 
   return (
