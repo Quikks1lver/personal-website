9c866a8b4c0dee6ec7663af750eddfc04f71ed09
Adam Fernandes <fernadam8@gmail.com>
2021-03-14 11:24:00 -0400

    cleaned css for projects more back button by plunking in app.css

--- frontend/src/ProjectsMore.tsx
+++ frontend/src/ProjectsMore.tsx
@@ -1,30 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MOBILE_WIDTH_THRESHOLD } from "./Constants";
-import { useMediaQuery } from "./useMediaQuery";
 
 /**
  * A "return to projects page" button present at the top of each individual project blurb
  * @returns
  */
 const ProjectsMore = () => {
-  const isMobile = useMediaQuery(MOBILE_WIDTH_THRESHOLD);
-
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "2%",
-        marginBottom: "3%",
-      }}
-    >
+    <div className="projects-more-back-button-container">
       <Link to="/projects">
-        <button
-          style={isMobile ? { fontSize: "40px" } : undefined}
-          className="projects-more-back-button"
-        >{` ↩ `}</button>
+        <button className="projects-more-back-button">{` ↩ `}</button>
       </Link>
     </div>
   );
