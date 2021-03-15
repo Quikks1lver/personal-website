f0ea04463f437b638def443a10642cdeaf184711
Adam Fernandes <57380803+Quikks1lver@users.noreply.github.com>
2021-03-15 00:00:43 -0400

    Hover Effects: Project Tiles & Back ↩ (#60)
    * cleaned css for projects more back button by plunking in app.css
    
    * hover for return to projects arrow
    
    * return back button hover set to {A} blue
    
    * z index for overlay text
    
    * overlay text & blur
    
    * overlay text css resource
    
    * more blur for tiles
    
    * removed tsc warning

--- frontend/src/ProjectsMore.tsx
+++ frontend/src/ProjectsMore.tsx
@@ -1,28 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { MOBILE_WIDTH_THRESHOLD } from "./Constants";
-import { useMediaQuery } from "./useMediaQuery";
 
 /**
  * A "return to projects page" button present at the top of each individual project blurb
  * @returns
  */
 const ProjectsMore = () => {
-  const isMobile = useMediaQuery(MOBILE_WIDTH_THRESHOLD);
+  const [isHoveredUpon, setIsHoveredUpon] = useState(false);
+  const hoveredStyle: React.CSSProperties = {
+    color: "#629ccc",
+  };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "2%",
-        marginBottom: "3%",
-      }}
-    >
+    <div className="projects-more-back-button-container">
       <Link to="/projects">
         <button
-          style={isMobile ? { fontSize: "40px" } : undefined}
+          style={isHoveredUpon ? hoveredStyle : undefined}
+          onMouseEnter={() => setIsHoveredUpon(true)}
+          onMouseLeave={() => setIsHoveredUpon(false)}
           className="projects-more-back-button"
         >{` ↩ `}</button>
       </Link>
