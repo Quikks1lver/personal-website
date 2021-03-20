8204344bbab5b8374f677fe7409dc1a465e9fab8
Adam Fernandes <57380803+Quikks1lver@users.noreply.github.com>
2021-03-20 17:48:19 -0400

    Typing Motion on Mini Headings  (#65)
    * react typing effect package
    
    * refactored mini heading
    
    * style constant
    
    * doc
    
    * resource

--- frontend/src/MiniHeading.tsx
+++ frontend/src/MiniHeading.tsx
@@ -1,15 +1,30 @@
 import React from "react";
+import ReactTypingEffect from "react-typing-effect";
 
 /**
- * A neat little heading component
+ * A neat little heading component, simulates someone typing
  * @param param0 string representing the heading
  */
 const MiniHeading = ({ heading }: { heading: string }) => {
-  const styles: React.CSSProperties = {
-    fontSize: 21,
+  const containerStyle: React.CSSProperties = {
+    fontSize: 22,
     textAlign: "center",
+    marginTop: 2,
+    marginBottom: 2,
+    padding: 2,
   };
-  return <h2 style={styles}>{`{ ${heading} }`}</h2>;
+  const typingSpeed: number = 200;
+
+  return (
+    <div style={containerStyle}>
+      <ReactTypingEffect
+        text={`{ ${heading} }`}
+        typingDelay={typingSpeed}
+        speed={typingSpeed}
+        eraseSpeed={750}
+      />
+    </div>
+  );
 };
 
 export default MiniHeading;
