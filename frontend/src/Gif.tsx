048b70db3dd3fd5de685109fe396ca25c0bd25f4
Adam Fernandes <fernadam8@gmail.com>
2021-03-22 12:20:05 -0400

    gif component

---
+++ frontend/src/Gif.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { gifProp } from "./CustomTypes";
+import { IPAD_WIDTH_THRESHOLD } from "./Constants";
+import { useMediaQuery } from "./useMediaQuery";
+
+/**
+ * Neatly renders a Gif to the screen
+ * @param param0 a gif prop, which needs a gif representing a filename to a video and alt text
+ * @returns
+ */
+const Gif = ({ gif, altText }: gifProp) => {
+  const isIpadOrSmaller = useMediaQuery(IPAD_WIDTH_THRESHOLD);
+
+  return (
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <img
+        style={{ borderRadius: "15px" }}
+        src={gif}
+        alt={altText}
+        width={isIpadOrSmaller ? "100%" : "50%"}
+        height="50%"
+      ></img>
+    </div>
+  );
+};
+
+export default Gif;
