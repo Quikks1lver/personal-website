da7c6ddf48b73f158b61467e9e0ea5dc48d76ea3
Adam Fernandes <fernadam8@gmail.com>
2021-03-22 12:46:44 -0400

    added paragraphs to JB project page

--- frontend/src/JusticeBytes.tsx
+++ frontend/src/JusticeBytes.tsx
@@ -8,6 +8,14 @@ import githubLogo from "./images/github_logo.png";
 
 import jb_vid from "./clips/jb.gif";
 import Gif from "./Gif";
+import TerminalTextBox from "./TerminalTextBox";
+
+const paragraphs: string[] = [
+  "Justice Bytes was the project a group of friends and I worked on for ShellHacks 2020, the largest hackathon in Florida, headed by Florida International University.",
+  "The idea was essentially a search engine for social justice-related news: a central place where anyone could go to learn more.",
+  "While the project utilized a full stack, I primarily worked on the backend as a web-scraper, finding news articles which my teammates would supplement with their findings to display to the user. I also worked on some frontend styling",
+  "Feel free to check out the brief demo above, and check out the Devpost submission and our GitHub repo!",
+];
 
 /**
  * Project page for Justice Bytes
@@ -21,7 +29,11 @@ const JusticeBytes = () => {
         moreInfo={['"A Search Engine for Social Justice"', "ShellHacks 2020"]}
       />
 
+      <TerminalTextBox paragraphs={paragraphs.slice(0, 2)} />
+
       <Gif gif={jb_vid} altText="Justice Bytes video" />
+
+      <TerminalTextBox paragraphs={paragraphs.slice(3)} />
 
       <div className="link-box-container">
         <LinkBox
