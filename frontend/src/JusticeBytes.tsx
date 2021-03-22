bbdb371974d6d49d4a07b8ff62cbd80edfd1d5d2
Adam Fernandes <fernadam8@gmail.com>
2021-03-21 23:10:16 -0400

    jb linkboxes

--- frontend/src/JusticeBytes.tsx
+++ frontend/src/JusticeBytes.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import LinkBox from "./LinkBox";
 import MiniHeading from "./MiniHeading";
+
+// images
+import devpostLogo from "./images/devpost_logo.png";
+import githubLogo from "./images/github_logo.png";
 
 /**
  * Project page for Justice Bytes
@@ -12,6 +17,18 @@ const JusticeBytes = () => {
         heading="Justice Bytes"
         moreInfo='"A Search Engine for Social Justice"'
       />
+      <div className="link-box-container">
+        <LinkBox
+          url="https://devpost.com/software/justicebytes"
+          siteName="Devpost"
+          image={devpostLogo}
+        />
+        <LinkBox
+          url="https://github.com/Hevia/JusticeBytes"
+          siteName="JusticeBytes GitHub"
+          image={githubLogo}
+        />
+      </div>
     </div>
   );
 };
