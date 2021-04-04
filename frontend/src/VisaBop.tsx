56ccc511f5f0e7b2b61fa009299bf93f70385590
Adam Fernandes <fernadam8@gmail.com>
2021-04-04 17:26:20 -0400

    added visabop clip

--- frontend/src/VisaBop.tsx
+++ frontend/src/VisaBop.tsx
@@ -7,11 +7,12 @@ import TerminalTextBox from "./TerminalTextBox";
 // images & videos
 import devpostLogo from "./images/devpost_logo.png";
 import githubLogo from "./images/github_logo.png";
+import vb_vid from "./clips/vb.gif";
 
 const paragraphs: string[] = [
   "Visa Bop was the project a couple of friends and I worked on for SwampHacks 2020, headed by the University of Florida.",
   "The idea was a website to connect immigrants to the U.S. with relevant jobs, based on visa type.",
-  "I primarily worked on scraping Indeed.com for job postings, and sending the information back to the front end.",
+  "I primarily worked on scraping Indeed.com for job postings using Python's Beautiful Soup module; I sent this the information back to the front end.",
   "Feel free to view the brief demo of my part of the project, and check out our Devpost submission and our GitHub repo below!",
 ];
 
@@ -27,9 +28,9 @@ const VisaBop = () => {
         moreInfo={['"Matching Visas to Jobs"', "SwampHacks 2020"]}
       />
 
-      <TerminalTextBox paragraphs={paragraphs.slice(0, 2)} />
+      <TerminalTextBox paragraphs={paragraphs.slice(0, 3)} />
 
-      {/* <Media media={} altText="Visa Bop demo" /> */}
+      <Media media={vb_vid} altText="Visa Bop demo" />
 
       <TerminalTextBox paragraphs={paragraphs.slice(3)} />
 
