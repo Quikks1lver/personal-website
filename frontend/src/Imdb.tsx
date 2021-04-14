e428fa71202a10438ebb69dd8a872a2f909e68a0
Adam Fernandes <fernadam8@gmail.com>
2021-04-13 23:35:23 -0400

    motion imdb

--- frontend/src/Imdb.tsx
+++ frontend/src/Imdb.tsx
@@ -3,6 +3,8 @@ import Media from "./Media";
 import LinkBox from "./LinkBox";
 import MiniHeading from "./MiniHeading";
 import TerminalTextBox from "./TerminalTextBox";
+import { motion } from "framer-motion";
+import { pageVariant, pageTransition } from "./FramerVariants";
 
 // images & videos
 import githubLogo from "./images/github_logo.png";
@@ -20,7 +22,13 @@ const paragraphs: string[] = [
  */
 const Imdb = () => {
   return (
-    <div>
+    <motion.div
+      animate="in"
+      exit="out"
+      initial="out"
+      variants={pageVariant}
+      transition={pageTransition}
+    >
       <MiniHeading
         heading="IMDB Parsing"
         moreInfo={["Get Movie Recommendations!", "Dec. 2019"]}
@@ -39,7 +47,7 @@ const Imdb = () => {
           image={githubLogo}
         />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
