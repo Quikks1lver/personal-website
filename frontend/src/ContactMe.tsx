d1e8b507021f12c501d325cab74fd652e287327f
Adam Fernandes <57380803+Quikks1lver@users.noreply.github.com>
2021-09-25 21:53:23 -0400

    Removing EmailJS from ContactMe (#110)
    * removed emailjs form
    
    * removed remnants
    
    * added very simple contact me at email header

--- frontend/src/ContactMe.tsx
+++ frontend/src/ContactMe.tsx
@@ -1,64 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import MiniHeading from "./MiniHeading";
 import { motion } from "framer-motion";
 import { pageVariant, pageTransition } from "./FramerVariants";
 
-// Email JS
-import emailjs from "emailjs-com";
-import { EmailJS_INFO } from "./EmailJS_Keys";
-import sendNotification from "./Notification_Handlers";
-
 /**
  * Contact Me page
- * Much of the code here is from https://www.emailjs.com/docs/examples/reactjs/, thanks!
  * @returns
  */
 const ContactMe = () => {
-  const [isHoveredUpon, setIsHoveredUpon] = useState(false);
-  const hoveredStyle: React.CSSProperties = {
-    backgroundColor: "rgba(255, 245, 245, 0.927)",
-    color: "#de5f25",
-  };
-  const citeStyle: React.CSSProperties = {
-    textAlign: "center",
-    color: "gray",
-    marginTop: "2%",
-    marginBottom: "0%",
-  };
-
-  /**
-   * Sends an email through EmailJS and sends a notification to the user
-   * @param Event
-   */
-  const handleClick = (e: any) => {
-    sendNotification(
-      "Thank you!",
-      "Message Sent.",
-      "I will respond as soon as I can.",
-      10000
-    );
-
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        EmailJS_INFO.EMAIL_SERVICE,
-        EmailJS_INFO.EMAIL_TEMPLATE,
-        e.target,
-        EmailJS_INFO.USER_ID
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-
-    e.target.reset();
-  };
-
   return (
     <motion.div
       animate="in"
@@ -68,46 +17,10 @@ const ContactMe = () => {
       transition={pageTransition}
     >
       <MiniHeading heading="Contact Me" />
-      <form className="contact-form" onSubmit={handleClick}>
-        <input
-          className="contact-input"
-          type="hidden"
-          name="contact_number"
-          required={true}
-        />
-        <input
-          className="contact-input"
-          placeholder={"Name"}
-          type="text"
-          name="name"
-          required={true}
-        />
-        <input
-          className="contact-input"
-          placeholder={"Email Address"}
-          type="email"
-          name="email"
-          required={true}
-        />
-        <textarea
-          className="contact-input"
-          placeholder="Message"
-          name="message"
-          style={{ height: 60 }}
-          required={true}
-        />
-        <input
-          onMouseEnter={() => setIsHoveredUpon(true)}
-          onMouseLeave={() => setIsHoveredUpon(false)}
-          className="contact-submit"
-          type="submit"
-          value="Send"
-          style={isHoveredUpon ? hoveredStyle : undefined}
-        />
-      </form>
-      <h5 style={citeStyle}>
-        <em>Powered by EmailJS</em>
-      </h5>
+      <h2 style={{ textAlign: "center" }}>
+        Please reach me at{" "}
+        <a href="mailto: fernadam8@gmail.com">fernadam8@gmail.com</a>!
+      </h2>
     </motion.div>
   );
 };
