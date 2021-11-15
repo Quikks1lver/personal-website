c26cbc95c67eaff0731def2d0e3da59f361fa103
Adam Fernandes <fernadam8@gmail.com>
2021-11-14 21:08:38 -0500

    with a minor in stats

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -13,7 +13,7 @@ import Footer from "./Footer";
 
 const heading: string = "Welcome to my website!";
 const paragraphs: string[] = [
-  "I'm an aspiring Software Engineer, and currently a sophomore at the UCF Burnett Honors College, pursuing a B.S. in Computer Science and a Minor in Statistics.",
+  "I'm an aspiring Software Engineer, and currently a sophomore at the UCF Burnett Honors College, pursuing a B.S. in Computer Science with a Minor in Statistics.",
   "So far, I've accumulated solid experience through internships, research, projects, hackathons, and teaching assistantship -- but I'm always game to dive into more ;).",
   "Don't hesitate to reach out if you'd like to get in contact with me, and have fun poking around my website!",
 ];
