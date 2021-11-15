c69ed238ffd5c6545ade21c58f7672a4d1247872
Adam Fernandes <fernadam8@gmail.com>
2021-11-15 15:48:30 -0500

    changed responsive header for newer phones

--- frontend/src/Constants.tsx
+++ frontend/src/Constants.tsx
@@ -1,3 +1,3 @@
-export const MOBILE_WIDTH_THRESHOLD: string = "(max-width: 425px)";
+export const MOBILE_WIDTH_THRESHOLD: string = "(max-width: 428px)";
 export const IPAD_WIDTH_THRESHOLD: string = "(max-width: 800px)";
 export const IPHONE_VERTICAL_WIDTH_THRESHOLD: string = "(max-width: 379px)";
